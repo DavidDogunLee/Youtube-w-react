a5a951ecaee9d41f6c6401156ed397159c1ae3bd
David Lee <DavidLee8two7@gmail.com>
2018-05-20 12:37:30 -0700

    video list display

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+import YTSearch from 'youtube-api-search';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+const API_KEY = 'AIzaSyDlpduhx7ghDdKFkSuTVieDb1qAQDP3vz4';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'reactjs' }, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
