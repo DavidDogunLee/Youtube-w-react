9d0dcd5b1135fd40ad5143df1da49caf2857c8dd
David Lee <DavidLee8two7@gmail.com>
2018-05-19 20:01:04 -0700

    init

---
+++ src/index.js
@@ -0,0 +1,8 @@
+var react = require('react');
+var ReactDOM = require('react-dom');
+
+const App = () => {
+  return <div> Hi </div>;
+};
+
+ReactDOM.render(<App />, document.querySelector('.container'));
